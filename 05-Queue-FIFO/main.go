7ef7f7daece07a7ac95277e12b6a5fa2fda84ebb
Aditya Hajare <aditya43@gmail.com>
2020-04-16 08:15:37 +0530

    FIFO Queue using Slices in Go

---
+++ 05-Queue-FIFO/main.go
@@ -0,0 +1,39 @@
+// Queue - FIFO
+package main
+
+import "fmt"
+
+type Queue struct {
+	items []string
+}
+
+func (q *Queue) Enqueue(val string) {
+	q.items = append(q.items, val)
+}
+
+func (q *Queue) Dequeue() string {
+	if len(q.items) == 0 {
+		return "0 items left Queue to dequeue"
+	}
+
+	item, items := q.items[0], q.items[1:]
+
+	q.items = items
+
+	return item
+}
+
+func main() {
+	q := Queue{}
+
+	q.Enqueue("Adi")
+	q.Enqueue("Nishi")
+	q.Enqueue("John")
+	q.Enqueue("Jane")
+
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue()) // Trying to Dequeue non existent item
+}
