af5aad1e873bfe7d37bed894239c53e5467c6a4f
Aditya Hajare <aditya43@gmail.com>
2020-04-17 08:33:07 +0530

    Generating Channels

---
+++ 09-Generating-Channels/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func process2(ch chan int) {
+	n := rand.Intn(3000)
+
+	time.Sleep(time.Duration(n) * time.Millisecond)
+
+	ch <- n
+}
+
+func main() {
+	ch := make(chan int)
+
+	go process2(ch)
+
+	fmt.Println("Waiting for process")
+
+	n := <-ch
+
+	fmt.Printf("Process took %d milliseconds\n", n)
+}
